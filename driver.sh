441c9d2b61a89d138cd2f76485d9a42f82b197e3
Kailash Karthik S <kailashkarthik9@gmail.com>
2019-12-21 12:20:12 -0500

    Adding kaldi interface code to driver

--- driver.sh
+++ driver.sh
@@ -1,8 +1,8 @@
 ###
 # This is the main driver script to run the code for the project on Multi-Modal Emotion Detection
 # Title - Transfer Learning for Emotion Recognition
-# UNIs - ks3740, jyh2127, aa4461
-
+# UNIs - Kailash Karthik ks3740, Jessica Huynh jyh2127, Amith Ananthram aa4461
+# Date: 12/20/19
 # Abstract - Emotion recognition is a challenging task for multiple reasons – the abstract nature of human emotions,
 # the context-dependent relationship between words in text and their conveyed emotion, the multi-modal nature in which
 # humans exhibit and understand emotions, and the lack of large annotated datasets for training machine learning models.
@@ -40,14 +40,16 @@ cd MultiModalEmotionDetection || exit
 sh create_dirs.sh
 cd ..
 
-###
-# Run Jessica's Script 1 - that generates the xvectors
-# Make sure to cd back to the root directory
-###
 
+# Generate the xvectors
+cd kaldi/egs/voxceleb/v2 || exit
+bash overall_script.sh 0 0
+cd ../../../../
+
+# This is for 6th layer embeddings
 # Copy the created xvectors to the external code base's directory
-cp -a kaldi/path_to_meld_xvectors/. MultiModalEmotionDetection/meld/data/kaldi/
-cp -a kaldi/path_to_iemocap_xvectors/. MultiModalEmotionDetection/iemocap/data/kaldi/
+cp -a kaldi/egs/voxceleb/v2/meld/outputs/xvectors_train/. MultiModalEmotionDetection/meld/data/kaldi/
+cp -a kaldi/egs/voxceleb/v2/iemocap/outputs/xvectors_train/. MultiModalEmotionDetection/iemocap/data/kaldi/
 
 # Run the text processing code to create fine-tuned BERT and multi-modal embedding ark files
 echo "The follwing code expects that MELD and IEMOCAP datasets are downloaded and extracted into the following directories:"
@@ -57,16 +59,57 @@ sh run.sh
 cd ..
 
 # Copy the created ark files to kaldi for LDA-PLDA
-### Jessica please make sure the casing in multimodal matches what you want in the kaldi/path but please don't change the source directory name
-cp -a MultiModalEmotionDetection/meld/data/kaldi/Text/. kaldi/path
-cp -a MultiModalEmotionDetection/meld/data/kaldi/Speech/. kaldi/path
-cp -a MultiModalEmotionDetection/meld/data/kaldi/Multimodal/. kaldi/path
+cp -a MultiModalEmotionDetection/meld/data/kaldi/Text/. kaldi/egs/voxceleb/v2/arks/Text
+cp -a MultiModalEmotionDetection/meld/data/kaldi/Speech/. kaldi/egs/voxceleb/v2/arks/Speech
+cp -a MultiModalEmotionDetection/meld/data/kaldi/Multimodal/. kaldi/egs/voxceleb/v2/arks/MultiModal
+cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Text/. kaldi/egs/voxceleb/v2/arks/iemocap/Text
+cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Speech/. kaldi/egs/voxceleb/v2/arks/iemocap/Speech
+cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Multimodal/. kaldi/egs/voxceleb/v2/arks/iemocap/Multimodal
 
-cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Text/. kaldi/path
-cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Speech/. kaldi/path
-cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Multimodal/. kaldi/path
+# Clear the /kaldi directories from MultiModalEmotionDetection code
+rm -r MultiModalEmotionDetection/meld/data/kaldi/
+rm -r MultiModalEmotionDetection/iemocap/data/kaldi/
+rm -r MultiModalEmotionDetection/meld/data/kaldi/Text/
+rm -r MultiModalEmotionDetection/meld/data/kaldi/Speech/
+rm -r MultiModalEmotionDetection/meld/data/kaldi/Multimodal/
+rm -r MultiModalEmotionDetection/iemocap/data/kaldi/Text/
+rm -r MultiModalEmotionDetection/iemocap/data/kaldi/Speech/
+rm -r MultiModalEmotionDetection/iemocap/data/kaldi/Multimodal/
+
+## This is for 8th layer embeddings from the neural network
+# Copy the created xvectors to the external code base’s directory
+cp -a kaldi/egs/voxceleb/v2/meld/outputs/xvectors_train_refined/. MultiModalEmotionDetection/meld/data/kaldi/
+cp -a kaldi/egs/voxceleb/v2/iemocap/outputs/xvectors_train_refined/. MultiModalEmotionDetection/iemocap/data/kaldi/
+
+# Run the text processing code to create fine-tuned BERT and multi-modal embedding ark files
+cd MultiModalEmotionDetection || exit
+sh run.sh
+cd ..
+
+# Copy the created ark files to kaldi for LDA-PLDA
+### Jessica please make sure the casing in multimodal matches what you want in the kaldi/path but please don’t change the source directory name
+cp -a MultiModalEmotionDetection/meld/data/kaldi/Text/. kaldi/egs/voxceleb/v2/arks2/Text
+cp -a MultiModalEmotionDetection/meld/data/kaldi/Speech/. kaldi/egs/voxceleb/v2/arks2/Speech
+cp -a MultiModalEmotionDetection/meld/data/kaldi/Multimodal/. kaldi/egs/voxceleb/v2/arks2/MultiModal
+cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Text/. kaldi/egs/voxceleb/v2/arks2/iemocap/Text
+cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Speech/. kaldi/egs/voxceleb/v2/arks2/iemocap/Speech
+cp -a MultiModalEmotionDetection/iemocap/data/kaldi/Multimodal/. kaldi/egs/voxceleb/v2/arks2/iemocap/Multimodal
 
 ###
 # Run Jessica's Script 2 - that performs LDA PLDA
 # Make sure to cd back to the root directory
+###
+cd kaldi/egs/voxceleb/v2 || exit
+bash overall_script.sh 4 1
+
+###
+# output will be in kaldi/egs/voxceleb/v2/output.txt
+# sample:
+# Speech on 6th layer embedding
+# EER: 47.21%
+# EER: 44.74%
+# EER: 43.75%
+# EER: 43.1%
+# EER: 41.66%
+# EER: 43.14%
 ###
